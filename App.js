603f4d5e40706330161e7a05d93df5e76f08864b
shiveshm18 <shivesh12mahajan@gmail.com>
2026-04-14 21:37:57 +0530

    foundation lecture

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div",{id: "heading"},
+//old way: React.createElemen => creates react element (js object) => html object on render
+/* const heading = React.createElement("div",{id: "heading"},
                 [React.createElement("h1",{key:1},"Fuck U Bitches"),
                  React.createElement("h1",{key:2},"piss off")   
-                ]);
+                ]); */
+
+// modern way: JSX => React.createElemen => creates react element (js object) => html object on render
+//  JSX is transpiled by bibal
+const heading = (
+    <h1 className="head">
+        Hello
+    </h1>
+);
+
+//React Component
+const Component2 = ()=> <h2>Bow before your king</h2>;
+
+const Component = ()=> (
+    <div>
+        {"hello"}
+        <h1 id="heading">I am the King of the World</h1>
+        <Component2 />
+    </div>
+    
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<Component />);
