20552b5fa1d8004f0f35f73224a929d493e0a9fb
shiveshm18 <shivesh12mahajan@gmail.com>
2026-04-14 06:33:02 +0530

    First commit in react

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div",{id: "heading"},
-                [React.createElement("h1",{},"Fuck U Bitches"),
-                 React.createElement("h1",{},"piss off")   
+                [React.createElement("h1",{key:1},"Fuck U Bitches"),
+                 React.createElement("h1",{key:2},"piss off")   
                 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
