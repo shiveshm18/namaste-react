e6957401784cda4cd14324f9f8398c0a4cef31bb
shiveshm18 <shivesh12mahajan@gmail.com>
2026-04-21 18:23:23 +0530

    using useState hook lec-5

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,23 @@
 import ResList from "../utils/mockData";
 import ResCard from "./ResCard";
+import { useState } from "react";
 
 const Body = ()=>{
+const [resList , setresList] = useState(ResList)
+
     return(
     <div className="body">
+        <div className="filters">
+            <button className="top-rated-btn"
+                onClick={()=> {
+                    const filtered = resList.filter((res)=> res.info.avgRating > 4.3);
+                    setresList(filtered);
+                }}>
+                Top Rated Restraunts 
+            </button>
+        </div>
         <div className="res-container">
-            { ResList.map((restraunts)=> <ResCard key={restraunts.info.id} ResData = {restraunts} />) }
-            
+            { resList.map((restraunts)=> <ResCard key={restraunts.info.id} ResData = {restraunts} />) }
         </div>
     </div>
 )};
